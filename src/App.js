7bdd532520bd92cae43acfabfd83c21b55687924
Alan Jones <pastorajsm@gmail.com>
2020-08-13 10:58:21 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import eagleLogo from "./assets/images/eagle.jpg";
+import bearLogo from "./assets/images/bears.jpg";
+import hornetLogo from "./assets/images/hornet.jpg";
+import ravenLogo from "./assets/images/raven.jpg";
+import Game from "./components/game/Game";
+import "./App.css";
 
-function App() {
+function App(props) {
+  const eagles = {
+    name: "Romulus Eagles",
+    logoSrc: eagleLogo,
+  };
+
+  const bears = {
+    name: "Belleville Bears",
+    logoSrc: bearLogo,
+  };
+
+  const hornets = {
+    name: "Dearborn Hornet",
+    logoSrc: hornetLogo,
+  };
+
+  const ravens = {
+    name: "Detroit Ravens",
+    logoSrc: ravenLogo,
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game venue="5120 S. Wayne Rd." homeTeam={eagles} visitingTeam={bears} />
+      <Game
+        venue="Romulus High Arena"
+        homeTeam={hornets}
+        visitingTeam={ravens}
+      />
     </div>
   );
 }
